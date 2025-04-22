983d9358b1745e47ab735e66f59472ddf4700aaa
Izukend <98744675+Izukend@users.noreply.github.com>
2025-04-22 15:35:10 +0200

    22/04/2025 First commit

---
+++ src/Components/CardSearchComponent.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Components;
+
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\Attribute\Computed;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+
+#[AsLiveComponent('card_search')]
+class CardSearchComponent
+{
+    #[LiveProp(writable: true)]
+    public string $query = '';
+
+    private HttpClientInterface $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    public function __invoke(): void
+    {
+    }
+
+    #[Computed]
+    public function results(): array
+    {
+        if (strlen($this->query) < 2) {
+            return [];
+        }
+        error_log("Recherche : " . $this->query);
+        $response = $this->client->request('GET', 'https://api.tcgdex.net/v2/fr/cards', [
+            'query' => ['name' => $this->query]
+        ]);  
+
+        return array_slice($response->toArray(), 0, 10);
+    }
+}
